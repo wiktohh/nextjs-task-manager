c828dc5812351873814f8c345e56efbf7e1f0234
wiktohh <wiktor.rzeznicki01@gmail.com>
2023-11-28 22:20:36 +0100

    create useAxios hook

--- app/hooks/use-axios.ts
+++ app/hooks/use-axios.ts
@@ -1,21 +1,28 @@
+"use client";
 import axios from "axios";
 
 const getToken = () => {
   const token = localStorage.getItem("token");
   return token;
 };
+
 export const useAxios = () => {
   const instance = axios.create({
     baseURL: "http://localhost:3000",
   });
+
   instance.interceptors.request.use(
     (config) => {
-      config.headers.Authorization = `Bearer ${getToken()}`;
+      const token = getToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
       return config;
     },
     (error) => {
       return Promise.reject(error);
     }
   );
+
   return instance;
 };
