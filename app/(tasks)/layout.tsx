70d499d61d6d5c95c40401e6a4027c1b7cf49873
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-01-08 14:04:45 +0100

    add metadata

--- app/(tasks)/layout.tsx
+++ app/(tasks)/layout.tsx
@@ -1,8 +1,13 @@
 import Header from "../components/Header/Header";
 import AuthProvider from "../context/auth-context";
+import {Metadata} from 'next'
 
 interface HomeLayoutProps {
   children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+  title: "TaskManager | Home",
 }
 
 const HomeLayout = ({ children }: HomeLayoutProps) => {
--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,8 +1,12 @@
+import { Metadata } from "next";
 import Header from "../components/Header/Header";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
+export const metadata: Metadata = {
+  title: "TaskManager | Auth",
+};
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
